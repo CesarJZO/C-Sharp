e3e3e2815c5b161025ab8097318874335594dc2f
CesarJZO <cesarjzo@outlook.com>
2022-05-01 18:01:59 -0500

    Initial commit

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿namespace CesarJZO
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                Console.WriteLine("Hello, world!");
+            else for (int i = 1; i < args.Length; i++)
+                Console.WriteLine($"Hello, {args[i]}!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# C#
+
+Even though I learned C# a while ago and I am used to write C# code, I want to do some experiments and learn more deeply the language, so here I will put the basics and advanced topics.
